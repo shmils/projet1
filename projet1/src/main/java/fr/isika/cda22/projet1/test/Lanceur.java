4134f05addfa26b42775340d50606c052ee2df59
shmils <milanyhussein@gmail.com>
2022-12-08 13:07:28 +0100

    adapt Lanceur to modifications

--- projet1/src/main/java/fr/isika/cda22/projet1/test/Lanceur.java
+++ projet1/src/main/java/fr/isika/cda22/projet1/test/Lanceur.java
@@ -5,36 +5,26 @@ import fr.isika.cda22.projet1.entites.*;
 public class Lanceur {
 
 	public static void main(String[] args) {
-		Stagiaire lilia = new Stagiaire("flici", "lilia", 78, "cda 22", 2022);
+		Stagiaire lilia = new Stagiaire("flici", "lilia", "78", "cda 22", "2022");
 		
 //		System.out.println(lilia);
 		
-		Stagiaire hossein = new Stagiaire("milani", "hossein", 45, "Al 22", 2022);
-		Stagiaire adrien = new Stagiaire("cerisier", "adrien", 94, "Al 22", 2022);
-		Stagiaire adrien1 = new Stagiaire("cerisier", "adrien", 95, "Al 22", 2022);
-		Stagiaire paul = new Stagiaire("paubel", "paul", 75, "Al 22", 2022);
+		Stagiaire hossein = new Stagiaire("milani", "hossein", "45", "Al 22", "2022");
+		Stagiaire adrien = new Stagiaire("cerisier", "adrien", "94", "Al 22", "2022");
+		Stagiaire adrien1 = new Stagiaire("cerisier", "adrien", "95", "Al 22", "2022");
+		Stagiaire paul = new Stagiaire("paubel", "paul", "75", "Al 22", "2022");
 		
-		Noeud n1 = new Noeud(lilia, null, null);
+		Noeud n1 = new Noeud(lilia);
 		
-//		System.out.println(n1);
+//		Arbre monArbre = new Arbre();
+//		System.out.println(monArbre);
+//		monArbre.ajouterStagiaire(adrien);
+//		monArbre.ajouterStagiaire(paul);
 		
-		n1.ajouterStagiaire(hossein);
-//		n1.ajouterStagiaire(adrien);
-//		n1.ajouterStagiaire(paul);
-//		n1.ajouterStagiaire(adrien1);
-		
-		Arbre monArbre = new Arbre();
-		System.out.println(monArbre);
-		monArbre.setRacine(n1);
-//		monArbre.ajouterStagiaire(lilia);
-//		System.out.println(n1);
-		monArbre.ajouterStagiaire(adrien);
-		monArbre.ajouterStagiaire(paul);
-		
-		System.out.println(monArbre);
-		System.out.println(monArbre.rechercherStagiaire(paul));
-		System.out.println(monArbre.rechercherStagiaire(hossein));
-		System.out.println(monArbre.rechercherStagiaire(adrien1));
+//		System.out.println(monArbre);
+//		System.out.println(monArbre.rechercherStagiaire(paul));
+//		System.out.println(monArbre.rechercherStagiaire(hossein));
+//		System.out.println(monArbre.rechercherStagiaire(adrien1));
 	}
 	
 }
