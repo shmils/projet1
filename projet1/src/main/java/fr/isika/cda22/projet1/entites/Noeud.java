6d9d3262985d772d39eeacb9b1430008a8271425
shmils <milanyhussein@gmail.com>
2022-12-08 12:31:51 +0100

    modify attributes of Noeud class to work with binary file

--- projet1/src/main/java/fr/isika/cda22/projet1/entites/Noeud.java
+++ projet1/src/main/java/fr/isika/cda22/projet1/entites/Noeud.java
@@ -2,19 +2,35 @@ package fr.isika.cda22.projet1.entites;
 
 public class Noeud {
 	
+	//attributs static
+	// Taille Stagiaire Octet + (nbAttributInt)*octet(int) (taille int en octet = 4)
+	private final static int TAILLE_NOEUD_OCTET = Stagiaire.getTailleStagiaireOctet() + 3*4;
+	
 	//attributs
 	private Stagiaire cle;
-	private Noeud filsGauche;
-	private Noeud filsDroit;
+	private int indiceFG;
+	private int indiceFD;
+	private int indiceDB;
 	
 	//constructeur
-	public Noeud(Stagiaire cle, Noeud filsGauche, Noeud filsDroit) {
+	public Noeud(Stagiaire cle, int indexFG, int indexFD, int indexDB) {
+		super();
 		this.cle = cle;
-		this.filsDroit = filsDroit;
-		this.filsGauche = filsGauche;
+		this.indiceFG = indexFG;
+		this.indiceFD = indexFD;
+		this.indiceDB = indexDB;
+	}
+	
+	public Noeud(Stagiaire cle) {
+		this(cle, -1, -1, -1);
 	}
 
 	//getters & setters
+	
+	public static int getTailleNoeudOctet() {
+		return TAILLE_NOEUD_OCTET;
+	}
+	
 	public Stagiaire getCle() {
 		return cle;
 	}
@@ -22,71 +38,79 @@ public class Noeud {
 	public void setCle(Stagiaire cle) {
 		this.cle = cle;
 	}
-
-	public Noeud getFilsGauche() {
-		return filsGauche;
-	}
-
-	public void setFilsGauche(Noeud filsGauche) {
-		this.filsGauche = filsGauche;
-	}
-
-	public Noeud getFilsDroit() {
-		return filsDroit;
-	}
-
-	public void setFilsDroit(Noeud filsDroit) {
-		this.filsDroit = filsDroit;
-	}
 	
+	public int getIndiceFG() {
+		return indiceFG;
+	}
+
+	public void setIndiceFG(int indiceFG) {
+		this.indiceFG = indiceFG;
+	}
+
+	public int getIndiceFD() {
+		return indiceFD;
+	}
+
+	public void setIndiceFD(int indiceFD) {
+		this.indiceFD = indiceFD;
+	}
+
+	public int getIndiceDB() {
+		return indiceDB;
+	}
+
+	public void setIndiceDB(int indiceDB) {
+		this.indiceDB = indiceDB;
+	}
+
 	//methodes specifique
-	public String toString() {
-		String s = "";
-		if(this.filsGauche != null) {
-			s += this.filsGauche.toString();
-		}
-		s += this.cle.toString();
-		if(this.filsDroit != null) {
-			s += this.filsDroit.toString();
-		}
-		return s;
-	}
+//	public String toString() {
+//		String s = "";
+//		if(this.filsGauche != null) {
+//			s += this.filsGauche.toString();
+//		}
+//		s += this.cle.toString();
+//		if(this.filsDroit != null) {
+//			s += this.filsDroit.toString();
+//		}
+//		return s;
+//	}
 	
-	public void ajouterStagiaire(Stagiaire nouveauStagiaire) {
-		if(this.cle.compareTo(nouveauStagiaire) < 0) {
-			if(this.filsDroit == null) {
-				this.filsDroit = new Noeud(nouveauStagiaire, null, null);
-			} else {
-				this.filsDroit.ajouterStagiaire(nouveauStagiaire);
-			}
-		} else if(this.cle.compareTo(nouveauStagiaire) > 0){
-			if(this.filsGauche == null) {
-				this.filsGauche  = new Noeud(nouveauStagiaire, null, null);
-			} else {
-				this.filsGauche.ajouterStagiaire(nouveauStagiaire);
-			}
-		} else {
-			System.out.println("!!!" + nouveauStagiaire + " existe déjà! T'es fatiguéeee !!!");
-		}
-	}
-
-	public Noeud rechercherStagiaire(Stagiaire stagiaire) {
-		if (this.cle.compareTo(stagiaire) == 0) {
-			return this;
-		} else if(this.cle.compareTo(stagiaire) < 0) {
-			if(this.filsDroit == null) {
-				return null;
-			} else {
-				return this.filsDroit.rechercherStagiaire(stagiaire);
-			}
-		} else {
-			if(this.filsGauche == null) {
-				return null;
-			} else {
-				return this.filsGauche.rechercherStagiaire(stagiaire);
-			}
-		}
-	}
+//	public void ajouterStagiaire(Stagiaire nouveauStagiaire) {
+//		if(this.cle.compareTo(nouveauStagiaire) < 0) {
+//			if(this.filsDroit == null) {
+//				this.filsDroit = new Noeud(nouveauStagiaire, null, null);
+//			} else {
+//				this.filsDroit.ajouterStagiaire(nouveauStagiaire);
+//			}
+//		} else if(this.cle.compareTo(nouveauStagiaire) > 0){
+//			if(this.filsGauche == null) {
+//				this.filsGauche  = new Noeud(nouveauStagiaire, null, null);
+//			} else {
+//				this.filsGauche.ajouterStagiaire(nouveauStagiaire);
+//			}
+//		} else {
+//			System.out.println("!!!" + nouveauStagiaire + " existe déjà! T'es fatiguéeee !!!");
+//		}
+//	}
+//
+//	public Noeud rechercherStagiaire(Stagiaire stagiaire) {
+//		if (this.cle.compareTo(stagiaire) == 0) {
+//			return this;
+//		} else if(this.cle.compareTo(stagiaire) < 0) {
+//			if(this.filsDroit == null) {
+//				return null;
+//			} else {
+//				return this.filsDroit.rechercherStagiaire(stagiaire);
+//			}
+//		} else {
+//			if(this.filsGauche == null) {
+//				return null;
+//			} else {
+//				return this.filsGauche.rechercherStagiaire(stagiaire);
+//			}
+//		}
+//	}
 	
 	
 }
