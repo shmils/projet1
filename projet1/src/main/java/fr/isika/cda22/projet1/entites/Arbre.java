8f40c764645783013f810e7afb90e4071c535cd2
shmils <milanyhussein@gmail.com>
2022-12-08 12:44:30 +0100

    modify attributes of class Arbre to work with binary files

--- projet1/src/main/java/fr/isika/cda22/projet1/entites/Arbre.java
+++ projet1/src/main/java/fr/isika/cda22/projet1/entites/Arbre.java
@@ -1,52 +1,62 @@
 package fr.isika.cda22.projet1.entites;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class Arbre {
 	
 	//attributs
-	private Noeud racine;
+	private RandomAccessFile raf;
+	private int indiceRacine;
 	
 	//constructeur
-	public Arbre() {
+	public Arbre(String fileName) throws IOException {
 		super();
-		this.racine = null;
+		raf = new RandomAccessFile(new File(fileName), "rw");
+		this.indiceRacine = 0;
 	}
 	
 	//getters & setters
-	public Noeud getRacine() {
-		return racine;
+	public RandomAccessFile getRaf() {
+		return raf;
 	}
 
-	public void setRacine(Noeud racine) {
-		this.racine = racine;
+	public void setRaf(RandomAccessFile raf) {
+		this.raf = raf;
 	}
 	
+	public int getIndiceRacine() {
+		return indiceRacine;
+	}
+
 	//methodes specifiques
-	public boolean isEmpty() {
-		return (this.racine == null);
-	}
-	
-	public String toString() {
-		if(this.isEmpty()) {
-			return "Arbre vide";
-		} else {
-			return this.racine.toString();
-		}
-	}
-	
-	public void ajouterStagiaire(Stagiaire nouveauStagiaire) {
-		if(this.isEmpty()) {
-			this.racine = new Noeud(nouveauStagiaire, null, null);
-		} else {
-			this.racine.ajouterStagiaire(nouveauStagiaire);
-		}
-	}
-	
-	public Noeud rechercherStagiaire(Stagiaire stagiaire) {
-		if(this.isEmpty()) {
-			return null;
-		} else {
-			return this.racine.rechercherStagiaire(stagiaire);
-		}
-	}
+//	public boolean isEmpty() {
+//		return (this.racine == null);
+//	}
+//	
+//	public String toString() {
+//		if(this.isEmpty()) {
+//			return "Arbre vide";
+//		} else {
+//			return this.racine.toString();
+//		}
+//	}
+//	
+//	public void ajouterStagiaire(Stagiaire nouveauStagiaire) {
+//		if(this.isEmpty()) {
+//			this.racine = new Noeud(nouveauStagiaire, null, null);
+//		} else {
+//			this.racine.ajouterStagiaire(nouveauStagiaire);
+//		}
+//	}
+//	
+//	public Noeud rechercherStagiaire(Stagiaire stagiaire) {
+//		if(this.isEmpty()) {
+//			return null;
+//		} else {
+//			return this.racine.rechercherStagiaire(stagiaire);
+//		}
+//	}
 	
 }
